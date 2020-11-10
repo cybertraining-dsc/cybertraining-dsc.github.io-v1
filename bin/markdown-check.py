d33c011b97a8e58e68a4468e5a5345b15b4de0e8
Gregor von Laszewski <laszewski@gmail.com>
2020-11-10 01:54:40 -0500

    markdown check

---
+++ bin/markdown-check.py
@@ -0,0 +1,108 @@
+#! /usr/bin/env python
+
+wrong = False
+
+def error(msg):
+    print ("ERROR:", msg)
+    wrong = True
+    
+import sys
+filename = sys.argv[1]
+
+with open(filename, 'r') as f:
+  content = f.read()
+
+#print (content)
+
+# check for numbers
+
+if "## 1." not in content:
+    error("You mut use numbered subsections")
+
+if "## 2." not in content or "## 3." not in content:
+    error("To few subsections in the report")
+
+if " http://" in content or " https://" in content:
+    error("http link is not enclosed in < >")
+
+for tag in ["ul", "img", "p", "b", "li"]:
+    if f"<{tag}>" in content or f"<//{tag}>" in content: 
+        error(f"html tag <{tag}> not allowed in project")
+    
+if " <http://" in content or " https://" in content:
+    error("http link is not enclosed in < >")
+
+if "## Abstract" not in content:
+    error("Abstract missing")
+
+if "Contents" not in content:
+    error("Contents missing")
+
+toc = """Contents
+
+{{< table_of_contents >}}
+
+{{% /pageinfo %}}
+"""
+
+for section in ["Conclusion", "Abstract", "Introduction"]:
+    if section not in content:
+        error(f"Could not find section {section}")
+    
+if toc not in content or "{{% pageinfo %}}" not in content:
+    error("pageinfo with Contents template is not properly used")
+
+if "] :" in content:
+    error ("spaces not allowed in front of :")
+
+if ".[^" in content or ". [^" in content:
+    error ("citation after a . not allowed")
+
+if "[^" not in content or "]:" not in content:
+    error("refernces are missing")
+    
+#check image inclusion
+
+counter = 0
+for line in content.splitlines():
+    line = line.strip()
+    counter = counter + 1 
+    if line.startswith("!["):
+        if "raw" not in line:
+            error("{counter}: image must be included from raw, url wrong")
+        ending = line.rsplit(".")[2][:-1]
+        if ending not in ["png", "jpg", "jpeg"]:
+            error(f"{counter}: Image ending must be one of png, jpg, jpeg")
+
+# check for multiple single hash
+
+count = 0
+for line in content.splitlines():
+    line = line.strip()
+    if line.startswith("# "):
+        count = count + 1
+if count > 1:
+    error("you used multiple titles")
+
+if "[Edit](https://github.com/cybertraining-dsc/" not in content:
+    error("no edit link found")
+
+if "**Keywords:**" not in content:
+    error("Keywords not found")
+
+section = False
+for line in content.splitlines():
+    line = line.strip()
+    counter = counter + 1     
+    if section and not line == "":
+        error(f"{counter}: line after heading must be empty")        
+    section = line.startswith("#")
+
+    
+print()     
+if wrong:
+    raise("This is not a valid report")
+else:
+    print("OK. No errors found. But there could be some as we do not test everything.")
+print()
+
